e44d3169c2ee62247f78fcb26978c4c0bab5b37b
jltato <84922122+jltato@users.noreply.github.com>
2025-04-05 10:16:19 -0300

    first commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SUAP_Movil
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Handlers;
+
+#if ANDROID
+using Android.Webkit;
+#endif
+
+namespace SUAP_Movil
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            #if ANDROID
+                        webView.HandlerChanged += (s, e) =>
+                        {
+                            var handler = webView.Handler as WebViewHandler;
+                            var nativeWebView = handler?.PlatformView as Android.Webkit.WebView;
+                            nativeWebView?.ClearCache(true);
+                            nativeWebView?.ClearHistory();
+                        };
+            #endif
+        }
+
+
+        protected override bool OnBackButtonPressed()
+        {
+            if (webView.CanGoBack)
+            {
+                webView.GoBack();
+                return true; // Cancelar la acción por defecto (salir de la app)
+            }
+
+            return base.OnBackButtonPressed(); // No hay historial, continuar con comportamiento por defecto
+        }
+
+
+
+
+
+
+
+}
+
+}
