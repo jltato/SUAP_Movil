9b584396072c0bbe4296ab42074047cdebacc2e3
jltato <84922122+jltato@users.noreply.github.com>
2025-04-06 22:32:05 -0300

    todo menos boton home

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,12 @@
 ﻿using Microsoft.Maui.Controls;
 using Microsoft.Maui.Handlers;
+using Android.Content;
+using SUAP_Movil.Platforms.Android;
+using Microsoft.Maui;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+
 
 #if ANDROID
 using Android.Webkit;
@@ -9,42 +16,110 @@ namespace SUAP_Movil
 {
     public partial class MainPage : ContentPage
     {
-       
+        private string currentUrl = string.Empty;
+
 
         public MainPage()
         {
             InitializeComponent();
 
-
-            #if ANDROID
-                        webView.HandlerChanged += (s, e) =>
-                        {
-                            var handler = webView.Handler as WebViewHandler;
-                            var nativeWebView = handler?.PlatformView as Android.Webkit.WebView;
-                            nativeWebView?.ClearCache(true);
-                            nativeWebView?.ClearHistory();
-                        };
-            #endif
         }
+
 
 
         protected override bool OnBackButtonPressed()
         {
+            
+            // Si estamos en login.aspx, desconectamos y salimos
+            if (currentUrl.Contains("login.aspx", StringComparison.OrdinalIgnoreCase))
+            {
+                MainActivity.Instance?.DisconnectVPN();
+                return base.OnBackButtonPressed(); // Cierra la app
+            }
+
+            // Si hay historial de navegación, volver atrás
             if (webView.CanGoBack)
             {
                 webView.GoBack();
-                return true; // Cancelar la acción por defecto (salir de la app)
+                return true;
             }
 
-            return base.OnBackButtonPressed(); // No hay historial, continuar con comportamiento por defecto
+            // Caso por defecto
+            return base.OnBackButtonPressed();
         }
 
 
+        protected override async void OnAppearing()
+        {
+            webView.Navigated += (s, e) =>
+            {
+                currentUrl = e.Url;
+            };
+
+            base.OnAppearing();
+
+            loadingIndicator.IsVisible = true;
+            loadingIndicator.IsRunning = true;
+
+            await WaitForVpnAndLoadWebView();
+
+           
+        }
+
+        private async Task WaitForVpnAndLoadWebView()
+        {
+            loadingIndicator.IsVisible = true;
+            webView.IsVisible = false;
+
+            bool vpnReady = false;
+            int retries = 10;
+
+            for (int i = 0; i < retries; i++)
+            {
+                if (await IsVpnHostReachable("192.44.30.11", 80))
+                {
+                    vpnReady = true;
+                    break;
+                }
+                else
+                {
+                    vpnReady = false;
+                  
+                }
+
+                await Task.Delay(1000);
+            }
+
+            if (vpnReady)
+            {
+               
+                // Luego navegar a la URL real
+                webView.Source = "http://192.44.30.11/login.aspx";
+                webView.IsVisible = true;
+                loadingIndicator.IsVisible = false;
+
+            }
+            else
+            {
+
+                await DisplayAlert("Error", "No se pudo alcanzar el servidor por VPN.", "OK");
+            }
+        }
 
 
-
-
-
-}
+        private async Task<bool> IsVpnHostReachable(string ipAddress, int port)
+        {
+            try
+            {
+                using var client = new TcpClient();
+                await client.ConnectAsync(ipAddress, port);
+                return client.Connected;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
 
 }
--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,11 +1,131 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Android.Widget;
+using Java.Lang;
 
-namespace SUAP_Movil
+
+namespace SUAP_Movil.Platforms.Android
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        // Definir el nombre del perfil
+        string profileName = "MiVPN"; // Reemplaza con el nombre de tu perfil
+        private bool isVpnConnected = false; // Controla el estado de la conexión VPN
+        public static MainActivity Instance { get; private set; }
+        //public static event Action OnVpnConnected; // Evento que notifica a MainPage
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Instance = this;
+            // Connectar la VPN cuando la aplicacion inicia
+            if (!isVpnConnected)
+            {
+                ConnectVPN();
+            }
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            // Solo intentar conectar la VPN si no está conectada
+            if (!isVpnConnected)
+            {
+                ConnectVPN();
+            }
+        }
+
+        private void ConnectVPN()
+        {
+            try
+            {
+                // Crear el Intent para conectar a OpenVPN
+                Intent shortcutIntent = new Intent(Intent.ActionMain);
+                shortcutIntent.SetClassName("de.blinkt.openvpn", "de.blinkt.openvpn.api.ConnectVPN");
+                shortcutIntent.PutExtra("de.blinkt.openvpn.api.profileName", profileName);
+
+                // Iniciar la actividad
+                StartActivity(shortcutIntent);
+                isVpnConnected = true;
+                //OnVpnConnected?.Invoke();
+            }
+            catch (ActivityNotFoundException e)
+            {
+                // Mostrar un Toast si no se encuentra la actividad
+                Toast.MakeText(this, "No se pudo encontrar la actividad de OpenVPN: " + e.Message, ToastLength.Long).Show();
+            }
+        }
+
+        public override void OnTrimMemory(TrimMemory level)
+        {
+            base.OnTrimMemory(level);
+
+            if (level == TrimMemory.UiHidden)
+            {
+                RunOnUiThread(() =>
+                {
+                    if (isVpnConnected)
+                    {
+                        DisconnectVPN(); // Llama mientras aún está visible
+                    }
+
+                    JavaSystem.Exit(0); // Termina la app
+                });
+            }
+        }
+
+        //protected override void OnStop()
+        //{
+        //    if (isVpnConnected)
+        //    {
+        //        DisconnectVPN();
+        //    }
+        //    JavaSystem.Exit(0); // Mata la app al irse a segundo plano
+
+        //    base.OnStop();
+           
+        //}
+
+        //protected override void OnDestroy()
+        //{
+        //    if (isVpnConnected)
+        //    {
+        //        DisconnectVPN();
+        //    }
+        //    JavaSystem.Exit(0);
+        //    base.OnDestroy();   
+        //}
+
+
+
+
+        // Método para desconectar la VPN cuando el usuario salga de la aplicación
+        public void DisconnectVPN()
+        {
+            try
+            {
+                Intent disconnectIntent = new Intent(Intent.ActionMain);
+                disconnectIntent.SetClassName("de.blinkt.openvpn", "de.blinkt.openvpn.api.DisconnectVPN");
+                disconnectIntent.PutExtra("de.blinkt.openvpn.api.profileName", profileName);
+
+                isVpnConnected = false;
+                StartActivity(disconnectIntent);
+               
+              
+            }
+            catch (ActivityNotFoundException e)
+            {
+                Toast.MakeText(this, "No se pudo encontrar la actividad de OpenVPN: " + e.Message, ToastLength.Long).Show();
+            }
+        }
+
+
+       
+
     }
 }
